9e7e951513a662dbdfa87f4f4b9c07ee55044a8f
Al Burns <al.burns.email@gmail.com>
2017-08-01 16:12:38 -0700

    played with regex

--- regex_maker.py
+++ regex_maker.py
@@ -4,24 +4,25 @@ import string
 regex_dict = {'digit': '\d', 'single_char': '[abc]'}
 
 
-def regex_gen_digit(unfiltered, regex_string):
-    regex_string += '\d'
-    unfiltered += str(random.randrange(0,10,1))
-    return unfiltered, regex_string
+def regex_gen_digit(selectme_string):
+    return str(random.randrange(0,10,1))
 
-def regex_gen_single_char(unfiltered, regex_string):
-    regex_string += '[abc]'
-    unfiltered += random.choice(string.ascii_letters)
-    return unfiltered, regex_string
-    
+def regex_gen_single_char(selectme_string, regex_string):
+    return random.choice(string.ascii_letters)
 
 def regex_gen():
-    unfiltered = ""
+    match_num = random.randrange(1, 5, 1)
+    selectme_string_list = []
+    for i in range(0, match_num):
+        selectme_string_list[i] = ""
     regex_string = ""
     for i in range(0, 10):
         regex_step = random.choice(list(regex_dict))
         if regex_step == "single_char":
-            unfiltered, regex_string = regex_gen_single_char(unfiltered, regex_string)
+            for i in range(0, match_num):
+                regex_string += '[abc]'
+                selectme_string_list[i] += regex_gen_single_char(selectme_string)
         elif regex_step == "digit":
-            unfiltered, regex_string = regex_gen_digit(unfiltered, regex_string)
-    print(unfiltered + '\n' + regex_string)
+            regex_string += '\d'
+            selectme_string, regex_string = regex_gen_digit(selectme_string, regex_string)
+    print(selectme_string + '\n' + regex_string)
